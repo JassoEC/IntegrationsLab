5aef8b79c62f93a2461a87b023712c4bc8f62474
Emanuel Campos <isw.ecamposj@gmail.com>
2026-04-20 13:58:48 -0600

    adds demo query

---
+++ supabase/snippets/Untitled query 215.sql
@@ -0,0 +1,4 @@
+truncate table messages restart identity cascade;
+truncate table webhook_events restart identity cascade;
+truncate table conversations restart identity cascade;
+truncate table contacts restart identity cascade;
